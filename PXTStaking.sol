e2e16cf80eabd7ba44127ec241906a44ccb7f099
top1st <riguang102@gmail.com>
2022-04-02 01:54:16 +0800

    PXTStaking

--- PXTStaking.sol
+++ PXTStaking.sol
@@ -4,34 +4,65 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "./PxswapFactory.sol";
 
 contract PXTStaking is Ownable {
     using SafeERC20 for IERC20;
 
     IERC20 public pxt;
+    IERC20 public matic;
+    address public lpAdmin;
+    address public feeAddress;
+    uint public withdrawFee; // PXT
+    uint public totalFee;
+    uint public totalDeposit; // PXT
+    mapping(uint => uint) public totalReward; // MATIC, LP, LP ...
+
+    bool enabled = true;
 
     struct UserInfo {
         uint amount;
-        mapping(uint => uint) rewardDebt;
+        uint rewardDebt;
+        uint reward;
     }
 
     struct PoolInfo {
         IERC20 rewardToken;
-        uint lastAmount;
-        uint newIncome;
-        uint totalIncome;
+        uint deposit;
         uint accTokenPerShare;
         uint minHoldRequest;
     }
 
     PoolInfo[] public poolInfo;
+    mapping(uint => mapping(address => UserInfo)) public userInfo;
 
-    mapping(address => UserInfo) public userInfo;
 
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
     event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
     event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+
+    constructor(IERC20 _pxt, IERC20 _matic, address _lpAdmin) {
+        pxt = _pxt;
+        matic = _matic;
+        lpAdmin = _lpAdmin;
+        poolInfo.push(PoolInfo({
+        rewardToken : _matic,
+        deposit : 0,
+        accTokenPerShare : 0,
+        minHoldRequest : 0
+        }));
+    }
+
+    function setLpAdmin(address admin) external onlyOwner {
+        lpAdmin = admin;
+    }
+
+    function setEnabled(bool action) external onlyOwner {
+        enabled = action;
+    }
+
+    function setFeeAddress(address feeAddr) external onlyOwner {
+        feeAddress = feeAddr;
+    }
 
     function massUpdatePools() public {
         uint256 length = poolInfo.length;
@@ -46,97 +77,162 @@ contract PXTStaking is Ownable {
         }
         poolInfo.push(PoolInfo({
         rewardToken : rewardToken,
-        lastAmount : 0,
-        totalIncome : 0,
+        deposit : 0,
         accTokenPerShare : 0,
         minHoldRequest : minHoldRequest
         }));
     }
 
     function setPoolInfo(uint pid, uint minHoldRequest) external onlyOwner {
+        require(pid > 0, "pid should gt 0");
         poolInfo[pid].minHoldRequest = minHoldRequest;
     }
 
-    function poolLength() public returns (uint) {return poolInfo.length;}
+    function setReplaceReward(uint pid, IERC20 rewardToken) external onlyOwner {
+        require(pid > 0, "pid should gt 0");
+        poolInfo[pid].rewardToken = rewardToken;
+    }
+
+    function setWithdrawFee(uint fee) external onlyOwner {
+        withdrawFee = fee;
+    }
+
+    function poolLength() public view returns (uint) {return poolInfo.length;}
 
     function updatePool(uint pid) public {
-        uint pxSupply = pxt.balanceOf(address(this));
-        uint curBalance = poolInfo[pid].rewardToken.balanceOf(address(this));
-        uint newIncome;
-        if (curBalance > poolInfo[pid].lastAmount) {
-            newIncome = curBalance - poolInfo[pid].lastAmount;
+        PoolInfo storage pool = poolInfo[pid];
+        if(pool.deposit == 0) {
+            return;
         }
-        poolInfo[pid].lastAmount = curBalance;
-        poolInfo[pid].accTokenPerShare = poolInfo[pid].accTokenPerShare + (newIncome * 1e12 / pxSupply);
+        uint adminBalance = pool.rewardToken.balanceOf(lpAdmin);
+        if (adminBalance > 0) {
+            pool.rewardToken.safeTransferFrom(lpAdmin, address(this), adminBalance);
+            pool.accTokenPerShare = pool.accTokenPerShare + (adminBalance * 1e12 / pool.deposit);
+        }
+    }
+
+    function sendToken(IERC20 token, address wallet) external onlyOwner {
+        require(enabled == false, "pool is active");
+        token.safeTransfer(wallet, token.balanceOf(address(this)));
     }
 
     function withdraw(uint pid, uint value) external {
-        UserInfo storage user = userInfo[msg.sender];
-        PoolInfo storage pool = poolInfo[pid];
+        require(pid > 0, "pid Should be gt 0");
+        require(value > withdrawFee, "Less Than Fee");
+        require(enabled == true, "pool disabled");
+        UserInfo storage user = userInfo[pid][msg.sender];
+        UserInfo storage user0 = userInfo[0][msg.sender];
+        uint pending;
         updatePool(pid);
+        updatePool(0);
 
-        if (user.amount > 0) {
-            uint pending = pool.accTokenPerShare * user.amount / 1e12 - user.rewardDebt[pid];
-            if (pending > 0) {
-                if(pid != 0) {
-                    require(pool.rewardToken.balanceOf(msg.sender) >= pool.minHoldRequest, "Less Holding Reward Balance");
-                }
-                pool.rewardToken.safeTransfer(msg.sender, pending);
+        require(poolInfo[pid].rewardToken.balanceOf(msg.sender) >= poolInfo[pid].minHoldRequest, "LP hold required" );
+
+        pending = poolInfo[pid].accTokenPerShare * user.amount / 1e12 - user.rewardDebt;
+        if (pending > 0) {
+            poolInfo[pid].rewardToken.safeTransfer(msg.sender, pending);
+            user.reward += pending;
+            totalReward[pid] += pending;
+        }
+
+        pending = poolInfo[0].accTokenPerShare * user0.amount / 1e12 - user0.rewardDebt;
+        if (pending > 0) {
+            poolInfo[0].rewardToken.safeTransfer(msg.sender, pending);
+            user0.reward += pending;
+            totalReward[0] += pending;
+        }
+
+        pxt.safeTransferFrom(lpAdmin, msg.sender, value - withdrawFee);
+        if(withdrawFee > 0) {
+            if(feeAddress != address (0)) {
+                pxt.safeTransferFrom(lpAdmin, feeAddress, withdrawFee);
             }
+            totalFee += withdrawFee;
         }
-        if (value > 0) {
-            user.amount -= value;
-            pxt.safeTransfer(msg.sender, value);
-        }
-        user.rewardDebt[pid] = pool.accTokenPerShare * user.amount / 1e12;
+        user.amount -= value;
+        poolInfo[pid].deposit -= value;
+
+        user0.amount -= value;
+        poolInfo[0].deposit -= value;
+        totalDeposit -= value;
+
+        user.rewardDebt = poolInfo[pid].accTokenPerShare * user.amount / 1e12;
+        user0.rewardDebt = poolInfo[0].accTokenPerShare * user0.amount / 1e12;
         emit Withdraw(msg.sender, pid, value);
     }
 
-    function deposit(uint value) external {
-
-        UserInfo storage user = userInfo[msg.sender];
-        uint pid = 0;
-        uint bal;
+    function deposit(uint pid, uint value) external {
+        require(pid > 0, "pid Should be gt 0");
+        require(enabled == true, "pool disabled");
+        UserInfo storage user = userInfo[pid][msg.sender];
+        UserInfo storage user0 = userInfo[0][msg.sender];
+        uint pending;
         updatePool(pid);
+        updatePool(0);
 
-        if (user.amount > 0) {
-            uint pending = poolInfo[pid].accTokenPerShare * user.amount / 1e12 - user.rewardDebt[pid];
-            if (pending > 0) {
-                poolInfo[pid].rewardToken.safeTransfer(msg.sender, pending);
-            }
+        require(poolInfo[pid].rewardToken.balanceOf(msg.sender) >= poolInfo[pid].minHoldRequest, "LP hold required");
+
+        pending = poolInfo[pid].accTokenPerShare * user.amount / 1e12 - user.rewardDebt;
+        if (pending > 0) {
+            poolInfo[pid].rewardToken.safeTransfer(msg.sender, pending);
+            user.reward += pending;
+            totalReward[pid] += pending;
         }
+
+        pending = poolInfo[0].accTokenPerShare * user0.amount / 1e12 - user0.rewardDebt;
+        if (pending > 0) {
+            poolInfo[0].rewardToken.safeTransfer(msg.sender, pending);
+            user0.reward += pending;
+            totalReward[0] += pending;
+        }
+
         if (value > 0) {
-            bal = pxt.balanceOf(address(this));
-            pxt.safeTransferFrom(msg.sender, address(this), value);
-            bal -= pxt.balanceOf(address(this));
-            user.amount += bal;
+            pxt.safeTransferFrom(msg.sender, lpAdmin, value);
+            user.amount += value;
+            poolInfo[pid].deposit += value;
+
+            user0.amount += value;
+            poolInfo[0].deposit += value;
+            totalDeposit += value;
         }
-        user.rewardDebt[pid] = poolInfo[pid].accTokenPerShare * user.amount / 1e12;
-        emit Deposit(msg.sender, pid, bal);
+
+        user.rewardDebt = user.amount * poolInfo[pid].accTokenPerShare / 1e12;
+        user0.rewardDebt = user0.amount * poolInfo[0].accTokenPerShare / 1e12;
+        emit Deposit(msg.sender, pid, value);
+
     }
 
     function emergencyWithdraw(uint256 pid) public {
-        UserInfo storage user = userInfo[msg.sender];
-        pxt.safeTransfer(msg.sender, user.amount);
-        emit EmergencyWithdraw(msg.sender, pid, user.amount);
-        user.amount = 0;
-        for(uint i = 0; i < poolInfo.length; i++) {
-            user.rewardDebt[i] = 0;
+        require(pid > 0, "pid should be gt 0");
+        UserInfo storage user = userInfo[pid][msg.sender];
+        UserInfo storage user0 = userInfo[0][msg.sender];
+        pxt.safeTransferFrom(lpAdmin, msg.sender, user.amount - withdrawFee);
+        if(withdrawFee > 0) {
+            if(feeAddress != address (0)) {
+                pxt.safeTransferFrom(lpAdmin, feeAddress, withdrawFee);
+            }
+            totalFee += withdrawFee;
         }
+
+        emit EmergencyWithdraw(msg.sender, pid, user.amount);
+
+        user0.amount -= user.amount;
+        poolInfo[0].deposit -= user.amount;
+        user0.rewardDebt = user0.amount * poolInfo[0].accTokenPerShare / 1e12;
+
+        poolInfo[pid].deposit -= user.amount;
+        user.amount = 0;
+        user.rewardDebt = 0;
     }
 
-
     function pendingReward(uint pid, address account) external view returns (uint) {
-        UserInfo storage user = userInfo[account];
-
-        uint pxSupply = pxt.balanceOf(address(this));
-        uint curBalance = poolInfo[pid].rewardToken.balanceOf(address(this));
-        uint newIncome;
-        if (curBalance > poolInfo[pid].lastAmount) {
-            newIncome = curBalance - poolInfo[pid].lastAmount;
+        UserInfo storage user = userInfo[pid][account];
+        PoolInfo storage pool = poolInfo[pid];
+        if(pool.deposit == 0) {
+            return 0;
         }
-        uint accPerShare = poolInfo[pid].accTokenPerShare + (newIncome * 1e12 / pxSupply);
-
-        return user.amount * accPerShare / 1e12 - user.rewardDebt[pid];
+        uint lpBalance = pool.rewardToken.balanceOf(lpAdmin);
+        uint accTokenPerShare = pool.accTokenPerShare + lpBalance * 1e12 / pool.deposit;
+        return user.amount * accTokenPerShare / 1e12 - user.rewardDebt;
     }
 }
